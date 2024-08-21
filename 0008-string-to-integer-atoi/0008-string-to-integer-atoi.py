77f2fa83ed14370de05c26970e90b0d00e1c7127
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-08-21 12:51:58 -0700

    Time: 39 ms (50.5%), Space: 16.5 MB (58.18%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,7 +1,6 @@
 import re
 class Solution:
     def myAtoi(self, s: str) -> int:
-        pattern ='[a-zA-Z.]'
         num = '[0-9]'
         new_s= ''
         new_i = 0
@@ -22,39 +21,5 @@ class Solution:
                 return 2**31-1
             elif  new_i<-(2**31):
                 return -(2**31)
-
-        # if len(s) == 0:
-        #     return 0
-        # elif re.match(pattern, s[0]):
-        #     return 0
-        # else:
-        #     for i,char in enumerate(s) :
-        #         # print("out",char)
-        #         if " " not in  char :
-        #             # print("btw",char)
-        #             if re.match(pattern, char):
-        #                 break
-        #             elif  re.match(num,char):
-        #                 # print("in",char)
-        #                 new_s+=char
-        #         if '-' in char:
-        #             if i > 0 and s[i - 1] == ' ':
-        #                 new_s+='-'+new_s
-        #             elif s[i]=="-" and i==0:
-        #                 if s[i+1] =="+":
-        #                     return 0
-        #                 else:
-        #                     new_s+='-'+new_s
-        #             else :
-        #                 return 0
-        # print(new_s)
-        
-        # if new_s:
-        #     new_i = int(new_s)
-        #     print(new_i)
-
-        # if  new_i>2**31-1 or new_i<-(2**31):
-        #     return -(2**31)
-        
         return new_i
         
