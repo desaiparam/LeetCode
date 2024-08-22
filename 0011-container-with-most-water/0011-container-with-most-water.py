e57180796ad0cd33a4904be484faf20732f1c274
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-08-21 17:14:56 -0700

    Time: 522 ms (57.02%), Space: 29.5 MB (45.2%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -9,11 +9,6 @@ class Solution:
                 left+=1
             else:
                 right-=1
-        # aream=0
-        # for i in range(len(height)):
-        #     for j in range(i + 1, len(height)):
-        #         aream = min(height[i],height[j]) *(j-i)
-        #         area =  max(area,aream)
         return area
 
         
