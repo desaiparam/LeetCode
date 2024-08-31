84a11076314869d7b54835222ee8aa970d9834a9
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-08-30 22:09:12 -0700

    Time: 10 ms (5.38%), Space: 43.2 MB (40.54%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> combi = new ArrayList<>();
+        helper(n, new StringBuilder(), combi,0,0);
+        return combi;
+    }
+    public void helper(int n , StringBuilder temp,List<String> combi,int openBrack,int closeBrack){
+        if(temp.length() == (n*2)){
+           combi.add(temp.toString());
+            return ;
+        }
+        // for(int i=start;i<n;i++){
+            // System.out.print("SDFGH");
+            // if(temp.size()<n){
+            if(openBrack<n){
+            temp.append("(");
+            System.out.println(openBrack);
+            helper(n,temp,combi,openBrack+1,closeBrack);
+            temp.deleteCharAt(temp.length() - 1);
+            }
+            // if(temp.contains("(")){
+            if(closeBrack<openBrack){
+                temp.append(")");
+                helper(n,temp,combi,openBrack,closeBrack+1);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        // }
+    }
+}
