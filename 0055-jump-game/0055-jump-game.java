7010b48ec0034b3a6a2fba0019aa142083f5ea5b
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-02 12:09:17 -0700

    Time: 639 ms (5.02%), Space: 45.9 MB (14.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,25 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean dp[] = new boolean[nums.length];
-        dp[0] = true;
-        for (int i = 1; i < nums.length; i++) {
-            // dp[i] = false;
-            for (int j = 0; j < i; j++) {
-                if(j+nums[j] >= i &&dp[j]){
-                    // System.out.println("EWRTYh");
-                    dp[i] = true;
-                    break;
-                } 
-            }
+        // boolean dp[] = new boolean[nums.length];
+        // dp[0] = true;
+        // for (int i = 1; i < nums.length; i++) {
+        //     // dp[i] = false;
+        //     for (int j = 0; j < i; j++) {
+        //         if(j+nums[j] >= i &&dp[j]){
+        //             // System.out.println("EWRTYh");
+        //             dp[i] = true;
+        //             break;
+        //         } 
+        //     }
+        // }
+        // return dp[nums.length - 1];
+
+        int ans =0;
+        for(int i =0;i<nums.length && i<=ans;i++){
+            ans = Math.max(ans,Math.min(i+nums[i],nums.length-1));
+        System.out.println("ans"+i);
         }
-        return dp[nums.length - 1];
+        return ans == nums.length-1;
     }
 }
 // }
