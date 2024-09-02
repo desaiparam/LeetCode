2803ebc704a51cb7c907257c5ab868e64022c697
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-02 12:03:06 -0700

    Time: 654 ms (5.02%), Space: 45.6 MB (58.4%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean dp[] = new boolean[nums.length];
+        dp[0] = true;
+        for (int i = 1; i < nums.length; i++) {
+            // dp[i] = false;
+            for (int j = 0; j < i; j++) {
+                if(j+nums[j] >= i &&dp[j]){
+                    // System.out.println("EWRTYh");
+                    dp[i] = true;
+                    break;
+                } 
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
+// }
+
+// if(nums.length==0){
+// return false;
+// }
+// if(nums.length<=1){
+// return true;
+// }
+
+// for(int i = 0;i<nums.length;i++){
+// if(nums.length-1 == nums[i]+i){
+// return true;
+// }
+// }
+
+// return false;
