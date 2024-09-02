580792c060b47dee561453e89ad3f8e30045e7f0
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-02 12:10:13 -0700

    Time: 4 ms (18.34%), Space: 46 MB (9.28%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,7 +3,6 @@ class Solution {
         int ans =0;
         for(int i =0;i<nums.length && i<=ans;i++){
             ans = Math.max(ans,Math.min(i+nums[i],nums.length-1));
-        System.out.println("ans"+i);
         }
         return ans == nums.length-1;
     }
