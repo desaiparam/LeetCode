4f747f8d904cd3de992ad9dcafba358d7a6801f0
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-12 21:16:29 -0700

    Time: 40 ms (17.32%), Space: 16.5 MB (40.86%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -1,10 +1,10 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         a = list(string.ascii_uppercase)
-        temp=[]
-        while columnNumber>0:
-            columnNumber=columnNumber-1
-            temp.append(a[(columnNumber%26)]) 
-            columnNumber=columnNumber//26
+        temp = []
+        while columnNumber > 0:
+            columnNumber = columnNumber - 1
+            temp.append(a[(columnNumber % 26)])
+            columnNumber = columnNumber // 26
         temp.reverse()
         return "".join(temp)
