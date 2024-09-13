2b9e070117c587c782e74eee4dfa4f0fd6ba68c0
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-12 22:59:04 -0700

    Time: 62 ms (11.81%), Space: 16.6 MB (94.57%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
@@ -1,34 +1,19 @@
 import math
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        # smaller = float('inf')
-        # larger = float('-inf')
-        # gcf=0
-        # for i in range(len(nums)):
-        #     # print(nums[i])
-        #     if(smaller>nums[i]):
-        #         smaller = nums[i]
-        #         print("Smaller:", smaller)
-        #     elif(larger<nums[i]):
-        #         larger = nums[i]
-        #         print("Larger:", larger)
-        #     if(smaller!=float('inf') and larger!=float('inf')):
-        #         return math.gcd(smaller,larger)
-        # return 1 
         smaller = float('inf')
         larger = float('-inf')
-
+        # gcf=0
         for i in range(len(nums)):
-            if nums[i] < smaller:
+            if smaller > nums[i]:
                 smaller = nums[i]
                 print("Smaller:", smaller)
-            if nums[i] > larger:
+            if larger < nums[i]:
                 larger = nums[i]
                 print("Larger:", larger)
+        if smaller!=float('inf') and larger!=float('-inf') :
+            return math.gcd(smaller,larger)
+        return 1 
 
-        # Compute the GCD only if smaller and larger are valid integers
-        if smaller != float('inf') and larger != float('-inf'):
-            return math.gcd(smaller, larger)
-            print("GCD of smaller and larger:", gcf)
-        else:
-            return 1
+
+      
