5e48070ce865209708680c5197d01323170f1b62
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-12 23:01:27 -0700

    Time: 54 ms (59.17%), Space: 16.7 MB (35.11%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
@@ -1,19 +1,27 @@
 import math
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        smaller = float('inf')
-        larger = float('-inf')
-        # gcf=0
-        for i in range(len(nums)):
-            if smaller > nums[i]:
-                smaller = nums[i]
-                print("Smaller:", smaller)
-            if larger < nums[i]:
-                larger = nums[i]
-                print("Larger:", larger)
-        if smaller!=float('inf') and larger!=float('-inf') :
-            return math.gcd(smaller,larger)
-        return 1 
+        # smaller = float('inf')
+        # larger = float('-inf')
+        # # gcf=0
+        # for i in range(len(nums)):
+        #     if smaller > nums[i]:
+        #         smaller = nums[i]
+        #         print("Larger:", larger)
+        #     if larger < nums[i]:
+        #         larger = nums[i]
+        #         print("Larger:", larger)
+        # if smaller!=float('inf') and larger!=float('-inf') :
+        #     return math.gcd(smaller,larger)
+        # return 1 
+        nums.sort()
+        print(nums)
+        smaller=nums[0]
+        larger=nums[-1]
+        print("Larger:", larger)
+        print("Smaller:", smaller)
+        return math.gcd(smaller,larger)
+        # return 1
 
 
       
