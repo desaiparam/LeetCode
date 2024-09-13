7b867cbf02939b12dc80bed596ce8fafec04f252
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-12 23:03:03 -0700

    Time: 59 ms (24.39%), Space: 16.8 MB (35.11%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
@@ -1,7 +1,22 @@
 import math
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        
+        #SOLUTION 1
+        # smaller = float('inf')
+        # larger = float('-inf')
+        # # gcf=0
+        # for i in range(len(nums)):
+        #     if smaller > nums[i]:
+        #         smaller = nums[i]
+        #         print("Larger:", larger)
+        #     if larger < nums[i]:
+        #         larger = nums[i]
+        #         print("Larger:", larger)
+        # if smaller!=float('inf') and larger!=float('-inf') :
+        #     return math.gcd(smaller,larger)
+        # return 1 
+
+        #Solution 2
         nums.sort()
         print(nums)
         smaller=nums[0]
@@ -9,5 +24,7 @@ class Solution:
         print("Larger:", larger)
         print("Smaller:", smaller)
         return math.gcd(smaller,larger)
+        # return 1
+
 
       
