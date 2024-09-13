d7e96ae8001557116efc3071dd241b945c0a4cfa
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-13 10:47:00 -0700

    Time: 77 ms (41.99%), Space: 16.6 MB (7.2%) - LeetHub

--- 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.py
@@ -1,15 +1,14 @@
 class Solution:
     def smallestEqual(self, nums: List[int]) -> int:
         smaller = min(nums)
-        # print(smaller)
         for i in range(len(nums)):
             print("i",i)
             if(i%10==nums[i]):
-                print("nums[i]",nums[i])
-                if(smaller<nums[i]):
-                    print("smaller",smaller)
-                    return i
-                else:
-                    return i
+                # print("nums[i]",nums[i])
+                # if(smaller<nums[i]):
+                #     print("smaller",smaller)
+                #     return i
+                # else:
+                return i
         
         return -1
