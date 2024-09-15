98c232bdd460c5ebad7266f63230cc5262456c3b
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-15 13:24:51 -0700

    Time: 36 ms (97.33%), Space: 16.4 MB (98.6%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -1,6 +1,8 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
+        #SOLUTION 1
         # return sum([i for i in range(1,n+1) if i % 3 ==0 or i % 5 ==0 or i % 7 ==0 ])
+        # SOLUTION 2
         def sum(k):
             j = n//k
             return j * k * (j+1)//2
