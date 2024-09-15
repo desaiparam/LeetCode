9b921aab21c30d2e7ae49a5005365929e72c4e5e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-15 12:58:53 -0700

    Time: 248 ms (5.14%), Space: 16.5 MB (47.3%) - LeetHub

---
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        sum =0
+        for i in range(1,n+1):
+            if i%3 == 0:
+                sum+=i
+                print("i 3",i)
+                print("sum 3",sum)
+            elif i%5 == 0:
+                sum+=i
+                print("i 5",i)
+                print("sum 5",sum)
+            elif i%7 == 0:
+                sum +=i
+                print("i 7",i)
+                print("sum 7",sum)
+        return sum
