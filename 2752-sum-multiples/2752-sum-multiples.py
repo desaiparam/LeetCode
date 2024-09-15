f6cea1c267a953379eb70b0c432e1e2024e19794
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-15 13:24:25 -0700

    Time: 41 ms (95.11%), Space: 16.4 MB (82.54%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -1,3 +1,8 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        return sum([i for i in range(1,n+1) if i % 3 ==0 or i % 5 ==0 or i % 7 ==0 ])
+        # return sum([i for i in range(1,n+1) if i % 3 ==0 or i % 5 ==0 or i % 7 ==0 ])
+        def sum(k):
+            j = n//k
+            return j * k * (j+1)//2
+        ans = sum(3) + sum(5) + sum(7) + sum(105) - sum(15) - sum(21) - sum(35)
+        return ans
