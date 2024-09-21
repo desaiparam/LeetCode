db4f2a5934946e0fdc65100dead9b40f5658f689
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-21 13:11:48 -0700

    Time: 35 ms (79.21%), Space: 16.5 MB (69.18%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.py
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.py
@@ -1,17 +1,19 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
+        # Solution 1
         setS = {}
         setT = {}
         diff = 0
         for i in range(len(s)):
             setS[s[i]] = i
-        print("setS",setS) 
+        # print("setS",setS) 
         for j in range(len(t)):
             setT[t[j]] = j
-        print("setT",setT) 
+        # print("setT",setT) 
         for key in setS :
             if key in setT:
                 diff += abs(setS[key] - setT[key])
         return diff
 
-        
+
+            
