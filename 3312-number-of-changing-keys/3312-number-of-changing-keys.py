b9deaf7b019dbbe3fd9cd1a1fa707738564f706a
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-27 10:32:08 -0700

    Time: 44 ms (14.69%), Space: 16.7 MB (20.07%) - LeetHub

---
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        l_s = s.lower()
+        print(l_s)
+        count = 0
+        key = l_s[0]
+        sets = {}
+        for i in range(len(l_s)):
+            if ord(l_s[i])- ord('a') != ord(key)-ord('a') :
+                key=l_s[i]
+                # sets[key] = 1
+                print(l_s[i])
+                count+=1
+        return count
+        
