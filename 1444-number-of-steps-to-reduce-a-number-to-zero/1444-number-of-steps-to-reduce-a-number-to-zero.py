c90b9f08bba3b2dff958106c1f717f45b7ee39f3
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-29 13:02:06 -0700

    Time: 30 ms (86.09%), Space: 16.5 MB (35.77%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,9 +4,9 @@ class Solution:
         while num > 0:
             if num % 2 == 0:
                 num = num//2
-                steps = steps +1
+                
             else:
                 num = num -1
-                steps  = steps +1
+            steps  = steps +1
         return steps
         
