b9b2b1d0e386f3f9da86badcf0e0645a43712d4a
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-29 12:48:06 -0700

    Time: 141 ms (73.13%), Space: 16.5 MB (44.13%) - LeetHub

--- 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.py
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.py
@@ -1,13 +1,15 @@
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
-        maxi = max(nums)
-        nums.remove(maxi)
-        sumy = 0
-        while k > 0:
-            sumy += maxi
-            nums.append(maxi+1)
-            maxi = max(nums)
-            nums.remove(maxi)
-            k-=1
-        return sumy
-        
+        #SOLUTION 1
+        # maxi = max(nums)
+        # nums.remove(maxi)
+        # sumy = 0
+        # while k > 0:
+        #     sumy += maxi
+        #     nums.append(maxi+1)
+        #     maxi = max(nums)
+        #     nums.remove(maxi)
+        #     k-=1
+        # return sumy
+        #SOLUTION 2
+        return max(nums)*k + (k*(k-1))//2
