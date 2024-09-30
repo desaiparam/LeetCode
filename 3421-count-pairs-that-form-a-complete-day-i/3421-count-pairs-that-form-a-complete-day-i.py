eb5eab6213e65a3dcb12efff5f5f2ac0b264b76f
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-30 13:15:46 -0700

    Time: 42 ms (95.86%), Space: 16.6 MB (16.22%) - LeetHub

--- 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.py
+++ 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.py
@@ -1,14 +1,28 @@
+from collections import defaultdict
 class Solution:
     def countCompleteDayPairs(self, hours: List[int]) -> int:
-        c = 0
-        for i in range(len(hours)):
-            for j in range(i+1,len(hours)):
-                if (hours[i] + hours[j] )% 24 == 0:
-                    c +=1
-            # elif hours[i] % 24 == 0:
-            #     print(hours[i])
-            #     c += 1
+       #SOULUTION 1
+        # c = 0
+        # for i in range(len(hours)):
+        #     for j in range(i+1,len(hours)):
+        #         if (hours[i] + hours[j] )% 24 == 0:
+        #             c +=1
+        #     # elif hours[i] % 24 == 0:
+        #     #     print(hours[i])
+        #     #     c += 1
        
-        return c 
+        # return c 
+
+        #SOLUTION 2 
+        remainder_c = defaultdict(int)
+        c = 0
+        for i in hours:
+            if i % 24 == 0:
+                c += remainder_c[0]
+            else:
+                c += remainder_c[24 - (i % 24)]
+            remainder_c[(i % 24)] += 1
+        return c
+
 
         
