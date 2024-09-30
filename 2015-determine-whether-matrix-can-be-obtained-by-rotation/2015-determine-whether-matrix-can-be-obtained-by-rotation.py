b2c930a921da838a36f4c5424715cd74020ef72f
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-09-30 09:50:06 -0700

    Time: 39 ms (88.72%), Space: 16.4 MB (80.22%) - LeetHub

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        def rotate(mat):
+            transpo = list(zip(*mat))
+            rotate = [list(reversed(row)) for row in transpo]
+            return rotate
+        rotated = rotate(mat)
+        Mn = len(rotated)
+        Mm = len(rotated[0])
+        Tn = len(target)
+        Tm = len(target[0])
+        # if Mn == None or Tn == None or Mn == 0 or Tn == 0 or Mn != Tn or Mm != Tm:
+        #     return False
+        # else:
+        for _ in range(4):
+                if rotated == target:
+                    return True
+                rotated=rotate(rotated)
+        return False        
