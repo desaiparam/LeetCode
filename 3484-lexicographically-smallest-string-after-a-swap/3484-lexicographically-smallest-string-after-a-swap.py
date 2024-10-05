10faa2c209259663c7bf3adfc7e8ad8b8466af0b
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-10-05 12:12:52 -0700

    Time: 37 ms (65.96%), Space: 16.6 MB (59.91%) - LeetHub

---
+++ 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSmallestString(self, s: str) -> str:
+        chars = list(s)
+        for i in range(len(s)-1):
+            # if int(s[i]) > int(s[i+1]):
+            if int(s[i]) % 2 == int(s[i+1]) % 2 and int(s[i]) > int(s[i+1]): 
+                # s = s[:i] + s[i + 1] + s[i] + s[i + 2:]
+                chars[i] , chars[i+1] = chars[i+1] , chars[i]
+                break
+        return ''.join(chars)
