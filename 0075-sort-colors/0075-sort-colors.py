5b5fa97a6bd036411e27bc5dbb0efdc29de7dd86
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-10-06 13:38:47 -0700

    Time: 39 ms (48.47%), Space: 16.5 MB (72.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,6 +1,30 @@
+from collections import deque
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+        #Solution 1 easy sorting
+        # nums.sort()
+
+        #Solution 2 using stack
+        stack = deque()
+        count_2 = 0
+        for i in range(len(nums)):
+            if nums[i] == 2:
+                count_2 += 1
+        print(count_2)
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                stack.append(nums[i])
+            if nums[i] == 0:
+                stack.appendleft(nums[i])
+            print(stack)
+        for i in range(len(nums)):
+            if i < count_2:
+                stack.append(2)
+            else:
+                stack.append
+        print(list(stack))
+        nums[:] = list(stack)
+
         """
         Do not return anything, modify nums in-place instead.
         """
