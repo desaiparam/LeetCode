424385cf3b02a1de391cd80683aaaf32b210d3ef
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-10-06 13:59:01 -0700

    Time: 33 ms (82.77%), Space: 16.7 MB (31.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,29 +1,50 @@
 from collections import deque
+from collections import defaultdict
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        #Solution 1 easy sorting
+        #Solution 1 easy sorting TC :O(nlogn) SC : O(n)
         # nums.sort()
 
-        #Solution 2 using stack
-        stack = deque()
-        count_2 = 0
+        #Solution 2 using stack O(n) O(n)
+        # stack = deque()
+        # count_2 = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 2:
+        #         count_2 += 1
+        # print(count_2)
+        # for i in range(len(nums)):
+        #     if nums[i] == 1:
+        #         stack.append(nums[i])
+        #     if nums[i] == 0:
+        #         stack.appendleft(nums[i])
+        #     print(stack)
+        # for i in range(len(nums)):
+        #     if i < count_2:
+        #         stack.append(2)
+        #     else:
+        #         stack.append
+        # print(list(stack))
+        # nums[:] = list(stack)
+
+        #Solution 3 using sets TC O(n) SC O(1)
+        # sets = {}
+        sets = defaultdict(int)
+        # if len(nums) <= 1:
+        #     return nums
         for i in range(len(nums)):
-            if nums[i] == 2:
-                count_2 += 1
-        print(count_2)
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                stack.append(nums[i])
-            if nums[i] == 0:
-                stack.appendleft(nums[i])
-            print(stack)
-        for i in range(len(nums)):
-            if i < count_2:
-                stack.append(2)
+            if nums[i] in sets:
+                sets[nums[i]] += 1
             else:
-                stack.append
-        print(list(stack))
-        nums[:] = list(stack)
+                sets[nums[i]] = 1
+        for i in range(len(nums)):
+            if i < sets[0]:
+                nums[i] = 0
+            elif i < sets[1] + sets[0]:
+                nums[i] = 1
+            else:
+                nums[i] = 2
+
+
 
         """
         Do not return anything, modify nums in-place instead.
