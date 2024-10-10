5a4c2492ef365f8d122a07f046bb0204fe18fbaa
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-10-10 09:49:55 -0700

    Time: 1387 ms (7.38%), Space: 58.7 MB (96.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,15 +4,6 @@ class Solution {
         int sellingStock = Integer.MIN_VALUE;
         int profit = 0;
         for (int i = 0; i < prices.length; i++) {
-            // System.out.println("buyingStock o"+prices[i]);
-            // if (buyingStock>prices[i]){
-            // buyingStock=prices[i];
-            // System.out.println("buyingStock i"+buyingStock);
-            // }
-            // else if (sellingStock<prices[i]){
-            // sellingStock=prices[i];
-            // System.out.println("sellingStock"+sellingStock);
-            // }
             if (buyingStock > prices[i]) {
                 buyingStock = prices[i];
                 System.out.println("buyingStock otto" + buyingStock);
@@ -21,14 +12,9 @@ class Solution {
                 if (currentProfit > profit) {
                     profit = currentProfit;
                 }
-                // profit = prices[i]-buyingStock;
             }
             System.out.println("buyingStock i" + profit);
         }
-        // if (sellingStock==Integer.MAX_VALUE || sellingStock==Integer.MIN_VALUE){
-        // return 0;
-        // }
-
         return profit;
     }
 }
