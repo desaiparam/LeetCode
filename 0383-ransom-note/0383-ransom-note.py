6ae2de587b26c3cba55ba60982e8b9e3a0ef2002
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-10-15 10:02:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,28 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        sets = {}
-        for i in range(len(magazine)):
-            if magazine[i] in sets:
-                sets[magazine[i]] += 1
-            else:
-                 sets[magazine[i]] = 1
-        print(sets)
-        # if ransomNote not in sets and len(ransomNote) <= len(sets):
-        #     return False
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in sets and sets[ransomNote[i]] > 0:
-                sets[ransomNote[i]] -=1
-            else:
+        # sets = {}
+        # for i in range(len(magazine)):
+        #     if magazine[i] in sets:
+        #         sets[magazine[i]] += 1
+        #     else:
+        #          sets[magazine[i]] = 1
+        # print(sets)
+        # # if ransomNote not in sets and len(ransomNote) <= len(sets):
+        # #     return False
+        # for i in range(len(ransomNote)):
+        #     if ransomNote[i] in sets and sets[ransomNote[i]] > 0:
+        #         sets[ransomNote[i]] -=1
+        #     else:
+        #         return False
+
+        # return True
+
+        for i in ransomNote:
+            if i not in magazine:
                 return False
-
+            else:
+                magazine = magazine.replace(i,"",1)
+                print(magazine)
         return True
-
 
         
