6be7e5501aba0dfdb18fcf3c60c517188865946d
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-10 16:50:15 -0800

    Time: 20 ms (85.23%), Space: 21.8 MB (55.84%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.maping = collections.defaultdict(int)
+
+    def add(self, key: int) -> None:
+        self.maping[key] = 1
+
+    def remove(self, key: int) -> None:
+        self.maping.pop(key,None)
+        # pass
+
+    def contains(self, key: int) -> bool:
+        return self.maping[key] == 1
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
