07570ffcac12a63c31fa9c6100c744dc26cb7f4b
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-16 14:24:22 -0800

    Time: 0 ms (100%), Space: 16.6 MB (59.96%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,16 +1,24 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        d = collections.defaultdict(list)
-        for i in jewels:
-            if i not in d:
-                d[i] = 1
-            else:
-                d[i] +=1
-        print(d)
+        
+        #SOLUTION 1 USING DEFAULTDICT
+        # d = collections.defaultdict(list)
+        # for i in jewels:
+        #     if i not in d:
+        #         d[i] = 1
+        #     else:
+        #         d[i] +=1
+        # print(d)
+        # c = 0
+        # for i in stones:
+        #     if i in d:
+        #         c+=1
+        # print(c)
+        # return c
+        #SOLUTION 2 USING SET
+        d = set(jewels)
         c = 0
         for i in stones:
             if i in d:
-                c+=1
-        print(c)
+                c +=1
         return c
-        
