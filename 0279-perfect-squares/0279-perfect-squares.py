ed448ecbc872421a4d20ace37ff46e89b3a4055a
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-19 11:02:03 -0800

    Time: 215 ms (90.1%), Space: 17.9 MB (25.37%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # ps = set()
+        ps = []
+        # print(n)
+        for i in range(n+1):
+            # print(i)
+            if int(i**0.5) * int(i**0.5) == i:
+                # print("i",i)
+                # ps.add(i)
+                ps.append(i)
+        visi = set()
+        # print(ps)
+        que = deque()
+        que.append((0,0))
+        while que:
+            target , ste = que.popleft()
+            if target == n:
+                return ste
+            # res = []
+            for i in ps:
+                # print(target)
+                n_target  = i+ target
+                # print(i,"i",n_target+target)
+                if n_target == n:
+                    return ste + 1
+                if n_target < n and n_target not in visi:
+                    visi.add(n_target)
+                    que.append((n_target,ste+1))
+                    # print(que)
+        # print(visi)
+        # return -1
+        
