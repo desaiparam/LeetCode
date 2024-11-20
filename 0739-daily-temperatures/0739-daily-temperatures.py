d60e1065fa3d15a71f81cfcde0e41641aa273504
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-20 10:40:37 -0800

    Time: 161 ms (8.27%), Space: 27.3 MB (73.69%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,29 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # stack = []
+        # que = deque()
+        # for i in range(len(temperatures)-1,-1,-1):
+        #     que.append((temperatures[i],0))
+        # ans = []
+        # # print(que)
+        # for idx , (value,lt) in enumerate(que):
+        #     while stack and stack[-1][0] <= value:
+        #         stack.pop()
+        #     if stack:
+        #         lt = idx - stack[-1][-1]
+        #     else:
+        #         lt = 0
+        #     stack.append((value,idx))
+        #     ans.append(lt)
+        # return ans[::-1]
         stack = []
         que = deque()
         for i in range(len(temperatures)-1,-1,-1):
-            que.append((temperatures[i],0))
+            que.append((temperatures[i]))
         ans = []
         # print(que)
-        for idx , (value,lt) in enumerate(que):
+        for idx , value in enumerate(que):
+            lt = 0
             while stack and stack[-1][0] <= value:
                 stack.pop()
             if stack:
