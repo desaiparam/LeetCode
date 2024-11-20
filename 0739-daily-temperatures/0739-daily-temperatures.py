aed37fdf297dad52c7cd418f3d5e6a91ff160bae
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-20 11:00:55 -0800

    Time: 72 ms (98.81%), Space: 25.6 MB (97.69%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -16,20 +16,18 @@ class Solution:
         #     stack.append((value,idx))
         #     ans.append(lt)
         # return ans[::-1]
-        stack = []
-        que = deque()
+        # stack = []
+        # que = deque()
+        ans = [0] * len(temperatures)
+        heigh = 0
+        print(heigh)
         for i in range(len(temperatures)-1,-1,-1):
-            que.append((temperatures[i]))
-        ans = []
-        # print(que)
-        for idx , value in enumerate(que):
-            lt = 0
-            while stack and stack[-1][0] <= value:
-                stack.pop()
-            if stack:
-                lt = idx - stack[-1][-1]
-            else:
-                lt = 0
-            stack.append((value,idx))
-            ans.append(lt)
-        return ans[::-1]
+            if heigh <= temperatures[i]:
+                # ans.append()
+                heigh = temperatures[i]
+                continue
+            day = 1
+            while temperatures[day + i] <= temperatures[i]:
+                day += ans[day + i]
+            ans[i] = day
+        return ans
