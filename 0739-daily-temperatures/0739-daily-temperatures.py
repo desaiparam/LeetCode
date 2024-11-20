1324d3c294eb4dfd840d7dd80367f8341f8697c9
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-20 10:38:46 -0800

    Time: 214 ms (5.05%), Space: 33.1 MB (12.95%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,20 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
-        # ans = []
-        # print(stack)
         que = deque()
         for i in range(len(temperatures)-1,-1,-1):
-            # print(temperatures[i])
             que.append((temperatures[i],0))
         ans = []
-        print(que)
-        # while que:
-        #     temp,lt = que.popleft()
-        
-            # print(temp-temp[-1])
-            # for i in range(len(temperatures)-1,-1,-1):
-            #     que.append(temperatures[i],lt+1)
+        # print(que)
         for idx , (value,lt) in enumerate(que):
             while stack and stack[-1][0] <= value:
                 stack.pop()
@@ -24,6 +15,4 @@ class Solution:
                 lt = 0
             stack.append((value,idx))
             ans.append(lt)
-            
-        # if
         return ans[::-1]
