1f40b3f8ad7a3751f8b337d3497fb70f5054aa87
Param Desai <110661906+desaiparam@users.noreply.github.com>
2024-11-23 15:18:39 -0800

    Time: 3 ms (65.04%), Space: 18.4 MB (20.74%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -3,24 +3,26 @@ class Solution:
         stack = []
         op = {'+', '-', '*','/'}
         for i in tokens:
-            stack.append(i)
+            # stack.append(i)
             if i in op:
-                if len(stack) >= 3:
-                    oper = stack.pop()
-                    f_v = int(stack.pop())
-                    s_v = int(stack.pop())
-                    if oper == '+':
-                        result = f_v + s_v
-                        stack.append(result)
-                    if oper == '-':
-                        result = s_v - f_v
-                        stack.append(result)
-                    if oper == '*':
-                        result = f_v * s_v
-                        stack.append(result)
-                    if oper == '/':
-                        result = s_v / f_v
-                        stack.append(result)
+                # if len(stack) >= 3:
+                    # i = stack.pop()
+                f_v = int(stack.pop())
+                s_v = int(stack.pop())
+                if i == '+':
+                    result = f_v + s_v
+                    # stack.append(result)
+                elif i == '-':
+                    result = s_v - f_v
+                    # stack.append(result)
+                elif i == '*':
+                    result = f_v * s_v
+                    # stack.append(result)
+                elif i == '/':
+                    result = s_v / f_v
+                stack.append(result)
+            else:
+                stack.append(i)
         return int(stack[-1])
 
 
