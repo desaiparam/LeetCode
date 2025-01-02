53b502f1366fa7756e81338d65648bd18fda50f0
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-01-02 11:33:44 +0530

    Time: 6 ms (58.92%), Space: 21.1 MB (37.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,27 +1,8 @@
+import heapq
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if len(nums) == k:
-            print("fewgrebg")
             return nums
-        d = {}
-        for i in nums:
-            if i not in d:
-                d[i] = 1
-            else:
-                d[i] +=1
-        d = dict(sorted(d.items(), key=lambda item: (-item[1], item[0])))
-        print(d)
-        ans = []
-        # for i , j in d.items():
-        #     if j > k:
-        #         ans.append(i)
-        i = 0
-        for j,key in d.items():
-            if i < k:
-                ans.append(j)
-                i+=1
-            else:
-                break
-        return ans
-
-        
+        count = Counter(nums)
+        return heapq.nlargest(k,count.keys(),key=count.get)
