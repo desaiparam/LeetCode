a0bb2a8ddcf6d72641eef8de3bc03bdff2075fe4
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-01-08 11:29:55 +0530

    Time: 84 ms (24.33%), Space: 18.9 MB (18.63%) - LeetHub

--- 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.py
+++ 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.py
@@ -1,23 +1,33 @@
 import heapq
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        if len(sticks) == 1:
-            return 0 
+        #SOLUTION 1 
+        # if len(sticks) == 1:
+        #     return 0 
+        # heapq.heapify(sticks)
+        # cost = 0
+        # while len(sticks) != 2:
+        #     f_s = heapq.heappop(sticks)
+        #     s_s = heapq.heappop(sticks)
+        #     cost = cost +f_s + s_s
+        #     heapq.heappush(sticks,f_s+s_s)
+        # #     print(sticks)
+        # # print(sticks)
+        # # print(cost)
+        # final = heapq.heappop(sticks)
+        # finals = heapq.heappop(sticks)
+        # # print(final)
+        # # print(finals)
+        # # print(cost+finals+final)
+        # return cost+finals+final
+        
+        #SOLUTION 2
         heapq.heapify(sticks)
         cost = 0
-        while len(sticks) != 2:
+        while len(sticks) > 1:
             f_s = heapq.heappop(sticks)
             s_s = heapq.heappop(sticks)
-            cost = cost +f_s + s_s
+            cost += (f_s + s_s)
             heapq.heappush(sticks,f_s+s_s)
-        #     print(sticks)
-        # print(sticks)
-        # print(cost)
-        final = heapq.heappop(sticks)
-        finals = heapq.heappop(sticks)
-        # print(final)
-        # print(finals)
-        # print(cost+finals+final)
-        return cost+finals+final
-        
-        
+
+        return cost
