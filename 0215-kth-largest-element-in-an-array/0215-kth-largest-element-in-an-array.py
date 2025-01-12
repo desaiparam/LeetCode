3da4e6a23e3c51667468ce43196b21231418371a
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-01-12 13:49:32 +0530

    Time: 98 ms (32.51%), Space: 28.7 MB (26.58%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,17 +1,26 @@
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        newN = [-num for num in nums]
-        heapq.heapify(newN)
-        print(newN)
-        # finalI = 0
-        while k != 1:
-            pop = heapq.heappop(newN)
-            print(pop)
-            # finalI = newN[0] * -1
-            print(newN[0])
-            k-=1
-        # return finalI
-        return (newN[0] * -1)
-        # print(newN[0] * -1)
+        #Solution 1
+        # newN = [-num for num in nums]
+        # heapq.heapify(newN)
+        # print(newN)
+        # # finalI = 0
+        # while k != 1:
+        #     pop = heapq.heappop(newN)
+        #     print(pop)
+        #     # finalI = newN[0] * -1
+        #     print(newN[0])
+        #     k-=1
+        # # return finalI
+        # return (newN[0] * -1)
+        # # print(newN[0] * -1)
+
+        #Solution 2
+        min_h = []
+        for num in nums:
+            heapq.heappush(min_h,num)
+            if len(min_h) > k:
+                heapq.heappop(min_h)
+        return min_h[0]
         
