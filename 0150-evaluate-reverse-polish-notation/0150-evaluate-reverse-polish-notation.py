1ae8703b269ede4082b55befcd4b74ed58a59451
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-03-09 13:30:08 -0700

    Time: 0 ms (100%), Space: 19.2 MB (17.5%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,24 +1,42 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        operations = {'+','-','*','/'}
+        # operations = {'+','-','*','/'}
         for i in tokens:
-            stack.append(i)
+            # stack.append(i)
             # print(stack)
-            if i in operations:
-                operator = stack.pop()
-                first_pop = int(stack.pop())
-                second_pop = int(stack.pop())
-                if operator == '+':
-                    result = first_pop + second_pop
-                    stack.append(result)
-                if operator == '-':
-                    result = second_pop - first_pop 
-                    stack.append(result)
-                if operator == '*':
-                    result = first_pop * second_pop
-                    stack.append(result)
-                if operator == '/':
-                    result = second_pop / first_pop
-                    stack.append(result)
-        return int(stack[-1])
+            # if i in operations:
+            # operator = stack.pop()
+            # first_pop = int(stack.pop())
+            # second_pop = int(stack.pop())
+            if i == '+':
+                result = stack.pop() + stack.pop()
+                stack.append(result)
+            elif i == '-':
+                a,b =  stack.pop() , stack.pop() 
+                stack.append(b-a)
+            elif i == '*':
+                result = stack.pop() * stack.pop()
+                stack.append(result)
+            elif i == '/':
+                a,b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+            else:
+                stack.append(int(i))
+        return int(stack[0])
+
+        # for c in tokens:
+        #     if c == '+':
+        #         stack.append(stack.pop() + stack.pop())
+        #     elif c == '-':
+        #         a, b = stack.pop(), stack.pop()
+        #         stack.append(b - a)
+        #     elif c == '*':
+        #         stack.append(stack.pop() * stack.pop())
+        #     elif c == '/':
+        #         a, b = stack.pop(), stack.pop()
+        #         stack.append(int(b / a))     
+        #     else:
+        #         stack.append(int(c))
+        # return stack[0]
+            
