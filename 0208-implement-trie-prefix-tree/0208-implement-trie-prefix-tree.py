4f7ec0ef160d87009e15e11da6e7a3c50c19d7ba
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-03-29 11:31:05 -0700

    Time: 81 ms (12.87%), Space: 35.4 MB (11.08%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,4 +1,4 @@
-class TrieNode():
+class TrieNode():#Creating a array to store characters and to check when we reach end of the word
     def __init__(self):
         self.arr = [None] * 26
         self.EndOfWord= False
@@ -11,22 +11,22 @@ class Trie:
         node = self.root
         for i in word:
             # if i in node:
-            index = ord(i) - ord('a')
-            if not node.arr[index]:
+            index = ord(i) - ord('a')#this checks the index of the character
+            if not node.arr[index]:#if not in arr we will insert in the trie
                 node.arr[index] = TrieNode()
             node = node.arr[index]
-        node.EndOfWord = True
+        node.EndOfWord = True#the word is completed
 
     def search(self, word: str) -> bool:
         node = self.root
         for i in word:
             index = ord(i) - ord('a')
-            if not node.arr[index]:
+            if not node.arr[index]:#not found in the array 
                 return False
-            node = node.arr[index]
+            node = node.arr[index]#if found check 
         return node.EndOfWord
             
-    def startsWith(self, prefix: str) -> bool:
+    def startsWith(self, prefix: str) -> bool:#same logic as search just return True 
         node = self.root
         for i in prefix:
             index = ord(i) - ord('a')
