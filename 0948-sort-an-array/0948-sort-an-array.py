e777edf62fb8af03514f75afe78ea36972e03b10
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-05-02 08:28:29 -0700

    Time: 627 ms (57.75%), Space: 25.7 MB (32.13%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        # Merge Sort
         if len(nums) <= 1:
             return nums
         pivot = len(nums)//2
@@ -20,5 +21,33 @@ class Solution:
         result.extend(left[left_pointer:])
         result.extend(right[right_pointer:])
         return result
+    # #Quick sort(this wont work as TC for test case 11 is O(n^2))
+    #     # print("waesdfgh")
+    #     self.quick(nums,0,len(nums) - 1)
+    #     return nums
+    # def quick(self,nums:List[int],low:int,high:int):
+    #     if low < high:
+    #         # print(low,"low")
+    #         # print(high,"high")
+    #         partitionIndex = self.partition(nums,low,high)
+    #         self.quick(nums,partitionIndex+1,high)
+    #         self.quick(nums,low,partitionIndex-1)
+    # def partition(self,nums:List[int],low:int,high:int):
+    #     # print("Sdfgh")
+    #     i = low
+    #     j = high
+    #     pivot = nums[low]
+    #     while i < j:
+    #         while nums[i] <= pivot and i <= high -1:
+    #             i+=1
+    #         while nums[j] > pivot and j >= low+ 1:
+    #             j-=1
+    #         if i < j:
+    #             nums[i],nums[j] = nums[j],nums[i]
+
+    #     nums[low],nums[j] = nums[j],nums[low]
+    #     return j
+
+
 
         
