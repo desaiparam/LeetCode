cf7fc930cc384d6e9a9c1c73a5f883ec56fc54df
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-07-03 08:44:06 -0700

    Time: 0 ms (100%), Space: 18.3 MB (8.69%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -7,6 +7,7 @@
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res = []
+        # this is to push the root inside the queue
         que = deque([root] if root else [])
         while que:
             level = []
@@ -17,6 +18,7 @@ class Solution:
                     que.append(node.left)
                 if node.right:
                     que.append(node.right)
+            # this the imp logic for the zigzag to work this checks the level of the tree and if even reverse the list
             level = list(reversed(level)) if len(res) % 2 else level
             res.append(level)
         return res
