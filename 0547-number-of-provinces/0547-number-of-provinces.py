0f2f9401ff8c0df0cb1bff6c4c0408bb20b15e81
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-07-14 08:38:55 -0700

    Time: 5 ms (65.28%), Space: 19.3 MB (27.93%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,21 +1,34 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+            #Solution 1 BFS TC:O(N^2) SC:O(N)
+            # count = 0
+            # n = len(isConnected)
+            # vis = [False] * n
+            # for i in range(n):
+            #     if not vis[i]:
+            #         q = deque([i])
+            #         vis[i] = True
+            #         count += 1
+            #         while q:
+            #             node = q.popleft()
+            #             for ne in range(n):
+            #                 if isConnected[node][ne] == 1 and not vis[ne]:
+            #                     q.append(ne)
+            #                     vis[ne] = True
+            # return count
+            vis = [False] * len(isConnected)
+            def dfs(root):
+                vis[root] = True
+                for n in range(len(isConnected)):
+                    if isConnected[root][n] ==1 and not vis[n]:
+                        dfs(n)
             count = 0
-            n = len(isConnected)
-            vis = [False] * n
-            for i in range(n):
-                if not vis[i]:
-                    q = deque([i])
-                    vis[i] = True
+            for i in range(len(isConnected)):
+                if  not vis[i]:
+                    dfs(i)
                     count += 1
-                    print("q",q,"count",count)
-                    while q:
-                        node = q.popleft()
-                        for ne in range(n):
-                            if isConnected[node][ne] == 1 and not vis[ne]:
-                                q.append(ne)
-                                vis[ne] = True
             return count
 
+            
 
         
