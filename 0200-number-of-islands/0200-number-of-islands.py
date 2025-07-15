6806d89c8d7a506af6e31b2b6207b68de398b561
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-07-15 08:52:29 -0700

    Time: 251 ms (51.25%), Space: 20.1 MB (82.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        que = deque()
-        visi = set()
-        nu = 0
+        count = 0
+        def dfs(i,j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == "1":
+                grid[i][j] = "0"
+                dfs(i+1,j)
+                dfs(i,j+1)
+                dfs(i-1,j)
+                dfs(i,j-1)
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visi:
-                    nu +=1
-                    que.append((i,j))
-                    visi.add((i, j))
-                    # print(que)
-                    # print(visi)
-                while que:
-                    c,k = que.popleft()
-                    for x,y in directions:
-                        nx,ny = x+c,y+k
-                        if 0 <= nx <len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == "1" and (nx,ny) not in visi:
-                            que.append((nx,ny))
-                            visi.add((nx,ny))
-
-        return nu
-
-        
+                if grid[i][j] == "1":
+                    dfs(i,j)
+                    count+=1
+        return count
         
