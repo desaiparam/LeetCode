8679210d96128d22b56a87dd1230c682de55097b
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-09-01 17:23:28 -0700

    Time: 204 ms (6.08%), Space: 19.1 MB (11.16%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -7,38 +7,46 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        trackParent = {}
-        def markParents(root,trackParent):
+        parent = {}
+        def markParent(root):
             q = deque([root])
             while q:
                 curr = q.popleft()
                 if curr.left:
-                    trackParent[curr.left] = curr
+                    parent[curr.left] = curr
                     q.append(curr.left)
                 if curr.right:
-                    trackParent[curr.right] = curr
+                    parent[curr.right] = curr
                     q.append(curr.right)
-        markParents(root,trackParent)
-        visited = set()
+        markParent(root)
         q = deque([target])
+
+        print(parent)
+        visited = set()
         visited.add(target)
-        curr_level = 0
+        currLevel = 0
         while q:
-            if curr_level == k:
+            if currLevel == k:
                 return [root.val for root in q]
-            curr_level += 1
-            for i in range(len(q)):
-                curr = q.popleft()
+            currLevel += 1      
+            for _ in range(len(q)):     
+                curr = q.popleft()        
                 if curr.left and curr.left not in visited:
                     q.append(curr.left)
                     visited.add(curr.left)
                 if curr.right and curr.right not in visited:
                     q.append(curr.right)
                     visited.add(curr.right)
-                if trackParent.get(curr) and trackParent.get(curr) not in visited:
-                    q.append(trackParent[curr])
-                    visited.add(trackParent[curr])
+                if parent.get(curr) and parent.get(curr) not in visited:
+                    q.append(parent[curr])
+                    visited.add(parent[curr])
         return []
 
-                
+
+
+
+
+
+
+
         
