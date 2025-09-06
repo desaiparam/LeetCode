e47b887dc95ce6b7766963c0a2bb0590c4e681b4
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-09-06 09:38:57 -0700

    Time: 387 ms (62.95%), Space: 18 MB (15.83%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,33 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        ans = []
-        for i in range(len(nums)):
+        res = []
+        for i in range(len(nums)-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            for j in range(i+1,len(nums)):
-                if j != i+1 and nums[j] == nums[j-1]:
+            for j in range(i+1,len(nums)-2):
+                if j > i + 1 and nums[j] == nums[j-1]:
                     continue
-                k = j+1
-                l = len(nums)-1
-                while k < l:
-                    sums = nums[i]
-                    sums = sums+nums[j]+nums[k] + nums[l]
-                    if sums == target:
-                        ans.append([nums[i],nums[j],nums[k],nums[l]])
-                        k+=1
-                        l-=1
-                        while k < l and nums[k] == nums[k-1]:
-                            k+=1
-                        while k < l and nums[l] == nums[l+1]:
-                            l-=1
-                    elif sums < target:
-                        k+=1
+                left = j + 1
+                right = len(nums) - 1
+                while left < right:
+                    curr = nums[i] + nums[j] + nums[left] + nums[right]
+                    # print(nums[i] , nums[j] , nums[left] , nums[right],curr)
+                    if curr == target:
+                        res.append([nums[i] , nums[j] , nums[left] , nums[right]])
+                        left += 1
+                        right -= 1
+                        while (left < right and nums[left] == nums[left - 1]):
+                            left += 1
+                        while (left < right and nums[right] == nums[right + 1]):
+                            right -= 1
+                    elif curr > target:
+                        right -= 1
                     else:
-                        l-=1
-        return ans
-
-
-                        
-
+                        left += 1
+        return res
         
