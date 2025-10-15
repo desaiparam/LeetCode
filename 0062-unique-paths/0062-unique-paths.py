527a7a677958df5c93bb53fa9a1da0cfc6621d96
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-15 14:22:09 -0700

    Time: 0 ms (100%), Space: 17.9 MB (46.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
+        memoisation = {}
         def helper(i,j):
-            if i == 0 and j == 0:
-                return 1 
-            if i < 0 or j < 0:
+            if i == 0 or j == 0:
+                return 1
+            if i < 0  or j < 0:
                 return 0
-            if (i ,j) in memo:
-                return memo[(i,j)]
-            # case of not taking the left side
-            notTake = helper(i,j-1)
-            # case of not taking the right side
-            take = helper(i-1,j)
-            memo[(i,j)] = notTake + take
-            return memo[(i,j)]
+            if (i,j)in  memoisation:
+                return memoisation[(i,j)]
+            case0 = helper(i,j-1)
+            case1 = helper(i-1,j)
+            memoisation[(i,j)] = case0 + case1
+            return memoisation[(i,j)]
         return helper(m-1,n-1)
-        
-
-        
-
-
-        
-        
