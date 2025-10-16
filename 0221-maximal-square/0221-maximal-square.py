17fcd3e812bead6e9bdfefd735830dde810ce489
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-16 16:04:13 -0700

    Time: 203 ms (26.19%), Space: 29.7 MB (92.07%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,12 +3,15 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         maxy = 0
-        dp = [[0] *(n+1) for _ in range(m+1)]
+        # dp = [[0] *(n+1) for _ in range(m+1)]
         for i in range(m-1,-1,-1):
             for j in range(n-1,-1,-1):
                 if matrix[i][j] == "1":
-                    dp[i][j] = 1 + min(dp[i+1][j],min(dp[i][j+1],dp[i+1][j+1]))
-                    maxy = max(maxy,dp[i][j])
+                    if i == m-1 or j == n-1:
+                        matrix[i][j] = 1
+                    else:
+                        matrix[i][j] = 1 + min(int(matrix[i][j+1]),min(int(matrix[i+1][j]),int(matrix[i+1][j+1])))
+                    maxy = max(maxy,matrix[i][j])
         return maxy*maxy
                         
 
