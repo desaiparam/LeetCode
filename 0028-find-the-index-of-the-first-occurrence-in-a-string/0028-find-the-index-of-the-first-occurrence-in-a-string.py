638dae254960825b8a884adf6582bf3f350475da
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-20 17:32:42 -0700

    Time: 0 ms (100%), Space: 17.7 MB (51%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,13 +1,38 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        i=0
-        if not needle :
-            return -1
-        while i<=len(haystack) - len(needle):
-            j=0
-            while j<len(needle) and haystack[i+j] == needle[j]:
-                j+=1
-            if j==len(needle):
-                return i
-            i+=1
-        return -1        
+        #    using KMP algo
+        n = len(needle)
+        lps = [0] * n
+        def preprocess(st):
+            lps[0] = 0
+            i = 1
+            j = 0
+            while i < n:
+                if st[i] == st[j]:
+                    j += 1
+                    lps[i] = j
+                    i += 1
+                elif st[i] != st[j] and j > 0:
+                    j = lps[j-1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        preprocess(needle)
+        i = 0 
+        j = 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+                if n == j:
+                    return i - j
+            elif haystack[i] != needle[j] and j > 0:
+                j = lps[j-1]
+            else:
+                i += 1
+        return -1
+
+
+
+
+
