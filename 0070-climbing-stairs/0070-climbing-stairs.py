5ed29a1c183a282614479ef63ed4baa24987d3f6
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-21 16:48:38 -0700

    Time: 0 ms (100%), Space: 17.7 MB (78.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        pr1=1
-        pr2=0
-        for i in range(1,n+1):
-            curi =pr1+pr2
-            print("curi",curi)
-            pr2=pr1
-            print("pr2",pr2)
-            print("pr1",pr1)
-            pr1 = curi
-        return pr1        
-
-                
-
-            
+        memo = [-1] * (n+1)
+        def helper(n):
+            if n == 0:
+                return 1
+            if memo[n] != -1:
+                return memo[n]
+            if n < 0:
+                return 0
+            memo[n] = helper(n-1) + helper(n-2)
+            return memo[n]
         
+        return helper(n)
