962a947959a74ce0a47eb8666237abb6cbdbb09e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-22 15:43:27 -0700

    Time: 4 ms (67.75%), Space: 18.6 MB (22.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        #TC O(N log n) and SC O(1) now for the follow up
+        # ans = [0]* (n+1)
+        # for i in range(n+1):
+        #     ans[i] = bin(i).count('1')
+        # return ans
+        # TC O(N) and no built in use
         ans = [0]* (n+1)
-        for i in range(n+1):
-            ans[i] = bin(i).count('1')
-        print(ans)
+        for i in range(1,n+1):
+            ans[i] = ans[i >> 1] + (i & 1) 
         return ans
         
