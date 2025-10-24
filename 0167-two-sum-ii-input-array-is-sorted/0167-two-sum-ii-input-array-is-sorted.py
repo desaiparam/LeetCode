7489919b7be9938fcfd0264e5569e995d03321f1
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-23 18:57:58 -0700

    Time: 3 ms (81.22%), Space: 18.6 MB (32.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,28 +1,35 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) -1
-        # print(j)
-        while i < j:
-            print(i,j)
-            if numbers[i] + numbers[j] == target:
-                print("1st ",i,j)
-                return [i+1,j+1]
-            mid = (i + j) // 2
-            # print("i",i)
-            # print("j",j)
-            # print(mid)
+        result= {}
+        for i in range(len(numbers)):
+            carry = target - numbers[i]
+            if carry in result:
+                return [result[carry],i+1]
+            result[numbers[i]] = i+1
+        # i = 0
+        # j = len(numbers) - 1
+        # # print(j)
+        # while i < j:
+        #     print(i,j)
+        #     if numbers[i] + numbers[j] == target:
+        #         print("1st ",i,j)
+        #         return [i+1,j+1]
+        #     mid = (i + j) // 2
+        #     # print("i",i)
+        #     # print("j",j)
+        #     # print(mid)
           
-            if numbers[j] + numbers[i] > target:
-                if numbers[mid] + numbers[i] <= target:
-                    j -= 1
-                else:
-                    j = mid
-            else:
-                if numbers[mid] + numbers[j] <= target:
-                    i = mid
-                else:
-                    i += 1
+        #     if numbers[j] + numbers[i] > target:
+        #         if numbers[mid] + numbers[i] <= target:
+        #             j -= 1
+        #         else:
+        #             j = mid
+        #     else:
+        #         if numbers[mid] + numbers[j] <= target:
+        #             i = mid
+        #         else:
+        #             i += 1
+
 
            
             
