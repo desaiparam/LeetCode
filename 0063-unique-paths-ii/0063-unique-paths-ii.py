90e20d4b2a30c35394c7887e85865333b953addb
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-24 17:27:19 -0700

    Time: 0 ms (100%), Space: 17.8 MB (91.48%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,18 +1,32 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        #TC and SC O(N*M)
+        # n = len(obstacleGrid)
+        # m = len(obstacleGrid[0])
+        # dp = [[0] * (m) for _ in range(n)]
+        # dp[n-1][m-1] = 1
+        # for i in  range(n-1,-1,-1):
+        #     for j in range(m-1,-1,-1):
+        #         if obstacleGrid[i][j] == 1:
+        #             dp[i][j] = 0
+        #         else:
+        #             if i < n-1: 
+        #                 # print(dp,i,j)
+        #                 dp[i][j] += dp[i+1][j]
+        #             if j < m-1:
+        #                 # print(dp[i][j+1])
+        #                 dp[i][j] += dp[i][j+1]
+        # return dp[0][0]
+        #TC O(N)
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
-        dp = [[0] * (m+1) for _ in range(n+1)]
-        dp[n-1][m-1] = 1
+        dp = [0] * m
+        dp[-1] = 1
         for i in  range(n-1,-1,-1):
             for j in range(m-1,-1,-1):
                 if obstacleGrid[i][j] == 1:
-                    dp[i][j] = 0
-                else:
-                    if i < n-1: 
+                    dp[j] = 0
+                elif j < m-1: 
                         # print(dp,i,j)
-                        dp[i][j] += dp[i+1][j]
-                    if j < m-1:
-                        # print(dp[i][j+1])
-                        dp[i][j] += dp[i][j+1]
-        return dp[0][0]
+                        dp[j] += dp[j+1]
+        return dp[0]
