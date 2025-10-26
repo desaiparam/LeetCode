7357d25abc443640cd84a729b758d7dd8f73e282
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-26 12:59:32 -0700

    Time: 0 ms (100%), Space: 18.6 MB (92.22%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,13 +1,33 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        #TC O(N^2) SC O(N^2)
+        # n = len(triangle)
+        # memo = {}
+        # def helper(i,j):
+        #     if i == n-1:
+        #         return triangle[i][j]
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     total = triangle[i][j] + min(helper(i+1,j), helper(i+1,j+1))
+        #     memo[(i,j)] = total
+        #     return total
+        # return helper(0,0)
+        #TC O(N^2) SC O(N^2)
+        # n = len(triangle)
+        # dp = [[float('inf')] * (n+1) for _ in range(n+1)]
+        # for i in range(n):
+        #     dp[n-1][i] = triangle[n-1][i]
+        # for i in range(n-2,-1,-1):
+        #     for j in range(i+1):
+        #         dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1])
+        # print(dp)
+        # return dp[0][0]
+        # print(n)
+        #TC O(N^2) SC O(N)
         n = len(triangle)
-        memo = {}
-        def helper(i,j):
-            if i == n-1:
-                return triangle[i][j]
-            if (i,j) in memo:
-                return memo[(i,j)]
-            total = triangle[i][j] + min(helper(i+1,j), helper(i+1,j+1))
-            memo[(i,j)] = total
-            return total
-        return helper(0,0)
+        dp = triangle[-1][:]
+        for i in range(n-2,-1,-1):
+            for j in range(i+1):
+                dp[j] = triangle[i][j] + min(dp[j],dp[j+1])
+        print(dp)
+        return dp[0]
