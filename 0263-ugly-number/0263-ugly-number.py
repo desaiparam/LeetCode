4fbcb558d7ac165b170c9a73fd6eea266ff4e664
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-27 18:25:23 -0700

    Time: 0 ms (100%), Space: 18 MB (24.07%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,16 +1,24 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        def helper(n):
-            if n <= 0:
-                return False
-            if n == 1:
-                return True
-            if n % 2 == 0:
-                return helper(n//2)
-            if n % 3 == 0:
-                return helper(n//3)
-            if n % 5 == 0:
-                return helper(n//5)
+        #TC and SC O(logn)
+        # def helper(n):
+        #     if n <= 0:
+        #         return False
+        #     if n == 1:
+        #         return True
+        #     if n % 2 == 0:
+        #         return helper(n//2)
+        #     if n % 3 == 0:
+        #         return helper(n//3)
+        #     if n % 5 == 0:
+        #         return helper(n//5)
+        #     return False
+        # return helper(n)
+        #TC O(N) SC O(1)
+        if n <= 0 :
             return False
-        return helper(n)
+        for mul in [2,3,5]:
+            while n % mul == 0:
+                n //= mul
+        return n == 1
         
