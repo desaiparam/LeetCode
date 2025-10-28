97d79e8368b865715e6c614a569c399945a898bc
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-28 12:16:32 -0700

    Time: 43 ms (31.6%), Space: 17.7 MB (57.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,14 +1,28 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        newX = str(x)
-        res = ""
-        if x < 0:
-            res += "-"
+        #TC and SC O(N)
+        # newX = str(x)
+        # res = ""
+        # if x < 0:
+        #     res += "-"
         
-        for i in range(len(newX)-1,-1,-1):
-            if newX[i] != "-":
-                res+= newX[i]
-        return int(res) if -2147483648<=int(res)<=2147483647  else 0
+        # for i in range(len(newX)-1,-1,-1):
+        #     if newX[i] != "-":
+        #         res+= newX[i]
+        # return int(res) if -2147483648<=int(res)<=2147483647  else 0
+        #TC O(N) SC O(1)
+        res = 0
+        newX=abs(x)
+        while newX != 0:
+            digit = newX % 10
+            res = res*10 + digit
+            newX //= 10
+        if x < 0:
+            res *= -1
+        return res if -2**31<= res <= 2**31 else 0
+        
+
+
 
         
         
