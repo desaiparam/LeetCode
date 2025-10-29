79f66841c76a3832326add73abefeff371d4f6c2
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-29 10:20:27 -0700

    Time: 35 ms (82.08%), Space: 18.1 MB (42.07%) - LeetHub

---
+++ 0284-peeking-iterator/0284-peeking-iterator.py
@@ -0,0 +1,54 @@
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        if iterator.hasNext():
+              self.nextElement = iterator.next()
+        else:
+            self.nextElement = None
+        
+        
+
+    def peek(self):
+        return self.nextElement
+
+    def next(self):
+        result = self.nextElement
+        if self.hasNext():
+              self.nextElement = self.iterator.next()
+        else:
+            self.nextElement = None
+        return result
+
+    def hasNext(self):
+        print(self.nextElement)
+        if self.nextElement != -100000:
+            return True
+        return False 
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
