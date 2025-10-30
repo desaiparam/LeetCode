53a7f94e3adf25f9ac3052be8d2b391dad81ebb6
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-30 12:10:59 -0700

    Time: 6 ms (61.71%), Space: 19.3 MB (87.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
+        # hashmap = {}
+        # for i in nums:
+        #     hashmap[i] = hashmap.get(i,0) + 1
+        # maxi = 0
+        # ans = 0
+        # for i in nums:
+        #     if maxi < hashmap[i]:
+        #         maxi = hashmap[i]
+        #         ans = i
+        # print(hashmap)
+        # return ans       
+        count = 0
+        element = 0
         for i in nums:
-            hashmap[i] = hashmap.get(i,0) + 1
-        maxi = 0
-        ans = 0
-        for i in nums:
-            if maxi < hashmap[i]:
-                maxi = hashmap[i]
-                ans = i
-        print(hashmap)
-        return ans       
-        
+            if count == 0:
+                element = i
+            if element == i:
+                count += 1
+            else:
+                count -= 1
+        return element
+
