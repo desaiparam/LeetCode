a9a5ad66afca0278d013fdd351679d6ebc66f38e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-10-31 17:55:11 -0700

    Time: 0 ms (100%), Space: 24.8 MB (44.78%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,7 +1,28 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        for i in nums:
-            if freq[i] == 1:
-                return i
+        #TC O(N) SC O(N)
+        # freq = Counter(nums)
+        # for i in nums:
+        #     if freq[i] == 1:
+        #         return i
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if nums[n-1] != nums[n-2]:
+            return nums[-1]
+        if nums[0] != nums[1]:
+            return nums[0]
+        low = 1
+        high = n-2
+        while low <= high:
+            mid = (low + high)//2
+            print(mid,low)
+            if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
+                return nums[mid]
+            if (mid % 2 == 0 and nums[mid] == nums[mid+1]) or (mid % 2 == 1 and nums[mid] == nums[mid-1]):
+                low = mid + 1
+            else:
+                high = mid -1
+        # return low
+    
         
