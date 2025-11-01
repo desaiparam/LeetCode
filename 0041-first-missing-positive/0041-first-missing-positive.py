0f01befb89ed0911b521b1d9ba03ea7a724cfd65
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-01 12:22:17 -0700

    Time: 17 ms (94.31%), Space: 34.8 MB (6.48%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,9 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # TC O(N) SC O(N)
         seen = set(nums)
-        for i in range(1,2**31-1):
+        for i in range(1,len(nums)+1):
             if i not in seen:
                 return i
+           
+        return len(nums) + 1
+            
+         
 
 
         
