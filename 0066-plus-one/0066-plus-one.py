cfc2c5534aea4d38465d14f05840899e19ae5f1d
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-01 13:58:17 -0700

    Time: 3 ms (3.98%), Space: 17.8 MB (30.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,10 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]<9:
-                digits[i]+=1
-                return digits
-            else:
-                digits[i]=0
-                # return digits
-        return [1]+digits
+        n = len(digits)
+        res = []
+        carry = 1
+        i = n - 1
+        # for i in range(len(digits)-1,-1,-1):
+        #     print(digits[i])
+        while i >= 0 or carry:
+            newA = digits[i] + carry
+            carry = newA //10
+            digits[i] = newA % 10
+            res.append(digits[i])
+            print(newA,carry,res)
+            i -= 1
+        res.reverse()
+        return res
+
+
