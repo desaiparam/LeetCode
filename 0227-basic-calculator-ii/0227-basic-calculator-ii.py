41470312fa2699ed879f611d917be580d434d5a3
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-02 10:10:36 -0800

    Time: 70 ms (54.1%), Space: 19 MB (82.04%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,27 +1,50 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        #TC O(N) SC O(N)
+        # currNum = 0
+        # stack = []
+        # operations = {"+","-","*","/"}
+        # lastOp = '+'
+        # for i in range(len(s)):
+        #     curr = s[i]
+        #     if curr.isdigit():
+        #         currNum = currNum * 10 + int(curr)
+        #     if curr in operations or i == len(s)- 1:
+        #         if lastOp == '*':
+        #             stack.append(stack.pop()*currNum)
+        #         elif lastOp == '/':
+        #             stack.append(int(stack.pop()/currNum))
+        #         elif lastOp == '+':
+        #             stack.append(currNum)
+        #         elif lastOp == '-':
+        #             stack.append(-currNum)
+        #         lastOp = curr 
+        #         currNum = 0
+        # return sum(stack)
+        #TC O(N) SC O(1)
+        tail = 0
+        calc = 0
+        lastsign = '+'
         currNum = 0
-        stack = []
-        operations = {"+","-","*","/"}
-        lastOp = '+'
         for i in range(len(s)):
             curr = s[i]
             if curr.isdigit():
                 currNum = currNum * 10 + int(curr)
-            # print(currNum)
-            if curr in operations or i == len(s)- 1:
-                if lastOp == '*':
-                    stack.append(stack.pop()*currNum)
-                elif lastOp == '/':
-                    print("gweg")
-                    stack.append(int(stack.pop()/currNum))
-                elif lastOp == '+':
-                    stack.append(currNum)
-                elif lastOp == '-':
-                    stack.append(-currNum)
-                lastOp = curr 
+            if (not curr.isdigit() and curr != ' ') or i == len(s) -1:
+                if lastsign == '+':
+                    calc += currNum
+                    tail = currNum
+                elif lastsign == '-':
+                    calc -= currNum
+                    tail = -currNum
+                elif lastsign == '*':
+                    calc = (calc - tail) + int(tail * currNum )
+                    tail = tail*currNum
+                elif lastsign == '/':
+                    calc = (calc - tail) + int(tail/currNum)
+                    tail = int(tail/currNum)
+                lastsign = curr
                 currNum = 0
-            
-            # print(stack)
-        return sum(stack)
+        return calc
+
      
