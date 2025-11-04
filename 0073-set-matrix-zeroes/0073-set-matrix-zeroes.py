9aab5c85f237a46f04a0a89441e974d40c123ef3
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-04 13:18:17 -0800

    Time: 0 ms (100%), Space: 18.4 MB (53.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,25 +3,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # matrix[0][..]
-        #matrix[..][0]
-        col0 = 1
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        n = len(matrix)
+        m = len(matrix[0])
+        setR = set()
+        setC = set()
+        for i in range(n):
+            for j in range(m):
                 if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    if j != 0:
-                        matrix[0][j] = 0
-                    else:
-                        col0 = 0
-        for i in range(1,len(matrix)):
-            for j in range(1,len(matrix[0])):
-                if matrix[i][j] != 0:
-                    if matrix[i][0] == 0 or matrix[0][j] == 0:
-                        matrix[i][j] = 0
-        if matrix[0][0] == 0:
-            for j in range(len(matrix[0])):
-                matrix[0][j] = 0
-        if col0 == 0:
-            for i in range(len(matrix)):
-                matrix[i][0] = 0
+                    setR.add(i)
+                    setC.add(j)
+        for i in range(n):
+            for j in range(m):
+                if i in setR or j in setC:
+                    matrix[i][j] = 0
