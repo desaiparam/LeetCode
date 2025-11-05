38ee8d67cedd3d50ba6045937ab812e5fbb1a145
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-05 10:36:15 -0800

    Time: 6 ms (47.28%), Space: 19 MB (9.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,10 +5,8 @@ class Solution:
         """
         i = 0
         j = 0
-        while(j < len(nums)):
+        while j < len(nums):
             if nums[j] != 0:
                 nums[i],nums[j] = nums[j],nums[i]
                 i += 1
             j += 1
-
-        
