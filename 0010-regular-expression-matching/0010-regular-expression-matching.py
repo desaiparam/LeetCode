d398eb7bceebdd995efa9739baceff636f8bb51e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-05 14:10:41 -0800

    Time: 3 ms (93.7%), Space: 18.2 MB (8.94%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        #TC O(2^(N+M)) OC (N+M)
+        # n = len(s)
+        # m = len(p)
+        # def helper(i,j):
+        #     if i == n and j== m:
+        #         return True
+        #     if j == m:
+        #         return False
+        #     if j + 1 < m and p[j+1] == "*":
+        #         if  i < n and (s[i] == p[j] or p[j] == "."): 
+        #            return  helper(i+1,j) or helper(i,j+2)
+        #         else:
+        #            return  helper(i,j+2)
+        #     elif i < n and ( s[i] == p[j] or p[j] == "." ):
+        #        return  helper(i+1,j+1)
+        #     return False
+        # return helper(0,0) 
+        m = len(p)
+        n = len(s)
+        memo = {}
+        def helper(i,j):
+            if i == n and j == m:
+                return True
+            if j == m:
+                return False
+            if (i,j) in memo:
+                return  memo[(i, j)]
+            if j + 1 < m and p[j+1] == "*":
+                if i < n and (s[i] == p[j] or p[j] == "."):
+                    memo[(i, j)] = helper(i+1,j) or helper(i,j+2) 
+                else:
+                    memo[(i, j)] = helper(i,j+2)  
+            elif i < n and( s[i] == p[j] or p[j] == "."):
+                memo[(i, j)] = helper(i+1,j+1)
+            else:
+                memo[(i, j)] = False
+                
+            return  memo[(i, j)]
+        return helper(0,0)
