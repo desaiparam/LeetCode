761c53df02c01ef0e01b69faaeb909636c8fe4fc
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-06 09:44:58 -0800

    Time: 4 ms (29.91%), Space: 17.7 MB (86.46%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
+        #TC O(log(sum(n)-max(n)).n) SC O(1)
+        # The concept over here is that the k is we will check the splits with maxsum possible from the array of max in the nums array and the total sum. This is the range of the array we will do bs and then find the possible sum which has the best value
         low = max(nums)
         high = sum(nums)
         def helper(maxsum):
@@ -11,12 +13,11 @@ class Solution:
                     sumy = i
                 else:
                     sumy += i
-            print(sumy)
             return split
         while low <= high:
             mid = low + (high - low) //2
             maxsumy = helper(mid)
-            if maxsumy<=k:
+            if maxsumy <= k:
                 high = mid - 1
             else:
                 low = mid + 1
