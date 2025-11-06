40dd70bc8151c36f12ca65d82c133e5cf89a9c43
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-06 09:42:36 -0800

    Time: 10 ms (74.74%), Space: 17.9 MB (49.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # sorted_s = sorted(s)
-        # sorted_t = sorted(t)
-        # print(sorted_s)
-        # print(sorted_t)
-        # return sorted_s == sorted_t
         if len(s) != len(t):
             return False
-        count = defaultdict(int)
-        for i in s:
-            count[i] += 1
-        for i in t:
-            count[i] -= 1
-        for val in count.values():
-            if val != 0:
-                return False
-        return True
+        hashmap1 = {}
+        hashmap2 = {}
+        for i in range(len(s)):
+            # if s[i] not in hashmap1:
+            #     hashmap1[s[i]] = 1
+            # else:
+            #     hashmap1[s[i]] += 1
+            # if t[i] not in hashmap2:
+            #     hashmap2[t[i]] = 1
+            # else:
+            #     hashmap2[t[i]] += 1
+            hashmap1[s[i]] = hashmap1.get(s[i],0) + 1
+            hashmap2[t[i]] = hashmap2.get(t[i],0) + 1
+        return hashmap1 == hashmap2
+        
+        
+        
