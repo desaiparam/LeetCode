20d518e884c54bd9e58eab1cd1f01534a073929e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-06 11:15:39 -0800

    Time: 826 ms (22.3%), Space: 20 MB (98.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        sum_l=[]
-        print(nums)
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] ==nums[i-1]:
+        res = []
+        n = len(nums)
+        for k in range(n):
+            if k > 0 and nums[k] == nums[k-1]:
                 continue
-            left,right = i+1,len(nums)-1
-            while left<right:
-                curr= nums[i] + nums[left] +nums[right]
-                if curr==0:
-                    sum_l.append([nums[i], nums[left],nums[right]])
-                    left+=1
-                    right-=1
-                    while left<right and nums[left] ==nums[left-1]:
-                        left+=1
-                    while left<right and nums[right]==nums[right+1]:
-                        right-=1
-                elif curr<0:
-                    left+=1
+            i = k + 1
+            j = n -1
+            while i < j:
+                if nums[i]+nums[k]+nums[j] == 0:
+                    res.append((nums[i],nums[k],nums[j]))
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+                elif nums[i]+nums[k]+nums[j] > 0:
+                    j -= 1
                 else:
-                    right-=1
-        return sum_l
-
+                    i += 1
+        return res
+                
+         
+        
