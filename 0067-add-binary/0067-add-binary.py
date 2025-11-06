d96ecdbabac679b11c040f73d7594a53e9a66d27
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-06 09:49:36 -0800

    Time: 3 ms (44.4%), Space: 17.5 MB (98.56%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,18 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        carr = 0
-        i = len(a) - 1
-        j = len(b) - 1
-        ans = []
-        while i >= 0 or j >= 0 or carr:
-            if i >= 0:
-                carr += int(a[i])
-                i -= 1
-            if j >= 0:
-                carr += int(b[j])
-                j -= 1
-            ans.append(str(carr %2 ))
-            carr //=2
-
-        return  ''.join(reversed(ans))
+        return "{0:b}".format(int(a, 2) + int(b, 2))
         
