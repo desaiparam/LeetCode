6f04e2a2071d10d9e0ee2aaefe147c7b30424cf2
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-11 13:21:24 -0800

    Time: 8 ms (33.6%), Space: 18.2 MB (98.94%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,49 +1,34 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        n = len(board)
+        m = len(board[0])
         if board[click[0]][click[1]] == 'M':
             board[click[0]][click[1]] = 'X'
             return board
-        m = len(board)
-        n = len(board[0])
-        q = deque()
-        q.append(click)
-        visit = set()
-        # visit
-        # visit.add(tuple(click))
+        
+        q = deque([(click[0],click[1])])
+        board[click[0]][click[1]] = 'B'
+        directions = [(0,1),(1,0),(-1,0),(0,-1),(-1,-1),(1,1),(1,-1),(-1,1)]
         while q:
             for _ in range(len(q)):
-                # print("q",q)
-                current = q.popleft()
-                x,y = current[0] , current[1]
-                count_mine = 0
-                for a,b, in [(0,1),(1,0),(0,-1),(-1,0),(-1,-1),(-1,1),(1,1),(1,-1)]:
-                    # print("a,b",a,b)
-                    nx,ny = x+a,y+b
-                    # print("nx",nx)
-                    # print("ny",ny)
-
-                    if 0 <= nx < m and 0 <= ny < n:
-                        # print("a,b",a,b)
-                        # print("board",board[nx][ny])
-                        if board[nx][ny] == 'M':
-                            count_mine += 1 
-                if count_mine > 0:
-                    # print(board[x][y])
-                    board[x][y] = str(count_mine)
+                r,c = q.popleft()
+                count = 0
+                for dr,dc in directions:
+                    nr = dr + r
+                    nc = c + dc
+                    if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 'M':
+                        count += 1
+                if count > 0:
+                    board[r][c] = str(count)
                 else:
-                    # print("board[x][y] before",board[x][y])
-                    board[x][y] = 'B'
-                    for a,b in [(0,1),(1,0),(0,-1),(-1,0),(-1,-1),(-1,1),(1,1),(1,-1)]:
-                        nx ,ny = x+a,y+b 
-                        # print("nx",nx)
-                        # print("ny",ny)    
-                        if 0 <= nx <m and 0 <= ny < n and (nx,ny) not in visit:
-                            q.append([nx,ny])
-                            visit.add((nx,ny))
-                        # print("q after",q)
-                        # print("visit",visit)
-                        # print("board[x][y] after",board[x][y])
+                    for dr,dc in directions:
+                        nr = dr + r
+                        nc = c + dc
+                        if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 'E':
+                            q.append((nr,nc))
+                            board[nr][nc] = 'B'
         return board
                     
 
-        
+
+
