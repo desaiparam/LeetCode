6df263527743ed36b1613a2094fa740a270e95e8
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-11-20 14:23:19 -0800

    Time: 51 ms (75.34%), Space: 18 MB (94.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -21,20 +21,34 @@ class Solution:
         #         memo[(i,j)] = res
         #     return res
         # return dfs(0,0)
+        #TC SC nm
+        # n = len(word1)
+        # m = len(word2)
+        # dp = [[0] * (m+1) for _ in range(n+1)]
+        # for i in range(1,m+1):
+        #     dp[0][i] = i
+        # for i in range(1,n+1):
+        #     for j in range(m+1):
+        #         if j == 0:
+        #             dp[i][j] = i
+        #             continue
+        #         if word1[i-1] == word2[j-1]:
+        #             dp[i][j] = dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+        # return dp[n][m]
         n = len(word1)
         m = len(word2)
-        dp = [[0] * (m+1) for _ in range(n+1)]
-        for i in range(1,m+1):
-            dp[0][i] = i
+        dp = [j for j in range(m + 1)]
         for i in range(1,n+1):
-            for j in range(m+1):
-                if j == 0:
-                    dp[i][j] = i
-                    continue
+            dia = dp[0]
+            dp[0] = i
+            for j in range(1,m+1):
+                temp = dp[j]
                 if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+                    dp[j] = dia
                 else:
-                    dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
-        return dp[n][m]
-            
+                    dp[j] = 1 + min(dp[j],dia,dp[j-1])
+                dia = temp
+        return dp[m]
         
