fe8ad6b4b6182f66a6acac861ae79372e76323b9
Param Desai <110661906+desaiparam@users.noreply.github.com>
2025-12-04 12:24:40 -0800

    Time: 102 ms (58.7%), Space: 18.4 MB (40.96%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        seenR = 0
+        seenS = 0
+        collision = 0
+        for i in directions:
+            if i == "R":
+                seenR += 1
+            elif i == "S":
+                collision += seenR
+                seenR = 0
+                seenS = 1
+            elif i == "L":
+                if seenR > 0:
+                    collision += seenR + 1
+                    seenR = 0
+                    seenS = 1
+                elif seenS == 1:
+                    collision += 1
+                    seenS = 1
+                
+        return collision
+        
