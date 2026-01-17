d6931551ede89e873ba13067d602c90b7f1b4f5e
Param Desai <110661906+desaiparam@users.noreply.github.com>
2026-01-17 10:06:10 -0800

    Time: 3 ms (100%), Space: 19.4 MB (5.56%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.py
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def countOfSubstrings(self, word: str, k: int) -> int:
+        #bad question do not do it again just think of sliding window that the window will keep shifting from left to right you have to track the vowels in between with constants and they do not cross k limit the vowel map keeps track of how many time the frequency has been repeated and with help of that we determine the distinct vowels present with the constants present as well 
         vowel = {'a':0,'e':0,'i':0,'o':0,'u':0}
         count = 0
         distinct = 0
