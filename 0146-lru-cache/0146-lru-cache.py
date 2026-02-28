4a5fcaab92f64b0b0f6348f42e2e750ff896c6a3
Param Desai <110661906+desaiparam@users.noreply.github.com>
2026-02-28 15:54:45 -0800

    Time: 119 ms (64.59%), Space: 78.9 MB (12.84%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,32 +1,30 @@
-class Node:
+class DDL:
     def __init__(self,key,val):
-        self.key = key
-        self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
+        self.val = val
+        self.key = key
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.size = capacity
         self.map = {}
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
+        self.head = DDL(-1,-1)
+        self.tail = DDL(-1,-1)
+        self.head.next=self.tail
+        self.tail.prev=self.head
+        
     def remove(self,node):
         node.prev.next = node.next
         node.next.prev = node.prev
-        node.prev = None
         node.next = None
-        
-
+        node.prev = None
     def addToHead(self,node):
         node.prev = self.head
         node.next = self.head.next
         node.next.prev = node
         self.head.next = node
-
+    
     def get(self, key: int) -> int:
         if key in self.map:
             node = self.map[key]
@@ -34,6 +32,8 @@ class LRUCache:
             self.addToHead(node)
             return node.val
         return -1
+
+
     def put(self, key: int, value: int) -> None:
         if key in self.map:
             node = self.map[key]
@@ -45,10 +45,15 @@ class LRUCache:
                 last = self.tail.prev
                 self.remove(last)
                 del self.map[last.key]
-            newNode = Node(key,value)
+            newNode = DDL(key,value)
             self.addToHead(newNode)
             self.map[key] = newNode
-        
+
+
+            
+            
+
+
 
 
 # Your LRUCache object will be instantiated and called as such:
